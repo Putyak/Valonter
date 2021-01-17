ebd7292c29e20fdfaa60e691e31c9de79f3d2f26
Putyak <ilya.putyak@gmail.com>
2021-01-17 18:34:33 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+import json
+import flask
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/districts/', methods=["GET"])
+def get_all_districts():
+    with open("districts.json") as f:
+        districts = json.load(f)
+    return flask.jsonify(districts)
+
+
+@app.route('/streets/', methods=["GET"])
+def get_streets():
+    district = request.args.get("district")
+    # Получение значение искомого района из запроса
+    with open("districts.json") as f:
+        data_districts = json.load(f)
+    # Открытие файла с списком районов и запись их в переменную
+    streets_data = data_districts[district]
+    # Получение искомого района в фале
+    streets_id = streets_data['streets']
+    # Получение массива улиц, которые надо найти
+    with open("streets.json") as f:
+        all_streets = json.load(f)
+    # Получение файла с списком улиц и запись их в переменную
+
+    street_list = []
+    for i in streets_id:
+        for each_street in all_streets:
+            if each_street[0] == i:
+                street_list.append(each_street)
+    # Пытаюсь пройти по всем улицам и записать их в новый массив
+
+    return jsonify(street_list)
+
+
+@app.route('/volunteers/', methods=["GET"])
+def get_volunteers():
+    streets = request.args.get("streets")
+    with open("streets.json") as f:
+        data = json.load(f)
+    return jsonify(data[streets])
+
+
+@app.route('/helpme/', methods=["POST"])
+def post_helpme():
+    data = flask.request.json
+    with open('helpme.json', 'w') as f:
+        json.dump(data, f)
+    return jsonify({"status": "success"}), 201
+
+
+if __name__ == '__main__':
+    app.run()
